73e72e45cadbee26580104dbe46df1e553c74ecd
Jan Olszewski <janolszewski1906@gmail.com>
2025-04-04 23:55:14 +0200

    update

--- embedding_service.py
+++ embedding_service.py
@@ -8,6 +8,12 @@ from tqdm import tqdm
 from typing import Dict, List, Union
 from rdkit import Chem
 
+HF_MODEL_NAMES = {
+    "GraphsGPT-1W": "DaizeDong/GraphsGPT-1W",
+    "GraphsGPT-2W": "DaizeDong/GraphsGPT-2W",
+    "GraphsGPT-4W": "DaizeDong/GraphsGPT-4W",
+    "GraphsGPT-8W": "DaizeDong/GraphsGPT-8W",}
+
 def batch_encode(tokenizer: GraphsGPTTokenizer, smiles_or_mol_list: List[Union[str, Chem.Mol]]) -> Union[Dict[str, torch.LongTensor], Dict[str, List[List]]]:
     batched_tokens = []
     for smiles_or_mol in smiles_or_mol_list:
@@ -36,11 +42,11 @@ class State:
 
 
 def setup(model_name: str, device: str, batch_size: int) -> None:
-    State.model = GraphsGPTForCausalLM.from_pretrained(model_name)
+    State.model = GraphsGPTForCausalLM.from_pretrained(HF_MODEL_NAMES[model_name])
     State.model.to(device)
     State.model.eval()
 
-    State.tokenizer = GraphsGPTTokenizer.from_pretrained(model_name)
+    State.tokenizer = GraphsGPTTokenizer.from_pretrained(HF_MODEL_NAMES[model_name])
     
     State.model_name = model_name
     State.device = device
